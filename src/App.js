71f192deaff6bb420db4c0f60a8fa6702ddd297d
Akash <Garfield@Akashs-MacBook-Pro.local>
2022-05-17 13:40:11 +0530

    second Commit

--- src/App.js
+++ src/App.js
@@ -2,65 +2,40 @@ import {useState,useEffect} from 'react';
 import Weather from './Component/Weather';
 import axios from 'axios';
 
-import './App.css';
 
 function App() {
   const [lat,setLat]=useState([]);
   const [long,setLong]=useState([]);
-  const [data,setData]=useState([]);
-
-
+  const [data,setData]=useState("");
+  
   useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
+      
+        await navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
       const temp = "https://api.openweathermap.org/data/2.5/weather/?lat=28.4451886&lon=77.5140147&units=metric&APPID=89d07fcddcc643e8775535d21dfb2e44"//`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`;
-      await axios(temp)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
+      await axios.get(temp)
+      .then(res=>{
+        setData(res.data)
+      })
+      if(data!=="")
+      {
+        console.log(data);
+      }
     }
     fetchData();
   }, [lat,long])
-  
+
   return (
+   
     <div className="App">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div></div>
-      )}
-      
+      {
+          (data!=="") ? <Weather weatherData={data}/> :<div></div>
+      }
     </div>
   );
-  
-  // useEffect(() => {
-  //   const fetchData = async () => {
-      
-  //       navigator.geolocation.getCurrentPosition(function(position) {
-  //       setLat(position.coords.latitude);
-  //       setLong(position.coords.longitude);
-  //     });
-  //     const temp = "https://api.openweathermap.org/data/2.5/weather/?lat=28.4451886&lon=77.5140147&units=metric&APPID=89d07fcddcc643e8775535d21dfb2e44"//`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`;
-  //     const response= await axios(temp);
-  //     //const json=response.json;
-  //     setData(response)
-  //     console.log(data);
-  //   }
-  //   fetchData();
-  // }, [lat,long])
-
-  // return (
-  //   <div className="App">
-  //     {
-  //         (data!==null) ? <Weather weatherData={data}/> : <div></div>
-  //     }
-  //   </div>
-  // );
 }
 
 export default App;
--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -1,25 +1,58 @@
-import React from 'react';
-
-import { Card } from 'semantic-ui-react'
-
-
-// const Weather = (props) =>{
-// <CardExampleCard weatherData={props.weather}/>
-// }
-
-// export default Weather;
+import React, {useState, useEffect } from 'react';
+import '../Style/Style.css';
 
 const Weather = (props) =>{
+    var today = new Date()
+    var curHr = today.getHours()
+
+    const [light,setLight]=useState("");
+    const [curr,setCurr]=useState(props.weatherData.main.temp);
+    const [sign,setSign]=useState(`\u00b0C`)
+   
+    useEffect(()=>{
+        if (curHr < 18) {
+            setLight("dayBackground");
+        } 
+        else {
+            setLight("nightBackground");
+        }
+    },[light])
+    
+    const handleTemp=()=>{
+         setCurr(props.weatherData.main.temp);
+         setSign(`\u00b0C`);
+    }
+    const handleHumidity=()=>{
+        setCurr(props.weatherData.main.humidity);
+        setSign(`%`);
+    }
+    const handleWind=()=>{
+        setCurr(props.weatherData.wind.speed);
+        setSign(`km/h`);
+    }
+   
     return(
-    <Card>
-      <Card.Content>
-          <Card.Header className="header">City Name: {props.weatherData.name}</Card.Header>
-          {/* <p>Temprature: {props.weatherData.data.main.temp}</p>  */}
-          {/* <p>Sunrise: {props.weatherData.sys.sunrise}</p>
-          <p>Sunset: {props.weatherData.data.sys.sunset}</p>
-          <p>Description: {props.weatherData.data.weather[0].description}</p> */}
-      </Card.Content>
-    </Card>
+       <div className={light}>
+           <div className='headerContent'>
+                <img className= "icon" src={`http://openweathermap.org/img/w/${props.weatherData.weather[0].icon}.png`} />
+                <h3 className="temp">{curr} {sign}</h3>
+                <div>
+                    <h5 className='humadity'>Humidity : {props.weatherData.main.humidity} %</h5>
+                    <h5 className='wind'>Wind : {props.weatherData.wind.speed} km/h</h5>
+                    
+                </div>
+           </div>
+           <div className='secondHeader'>
+               <p className='secondHeaderContent' onClick={handleTemp}><strong>Temperature</strong></p> 
+               <p className='secondHeaderContent' onClick={()=>handleHumidity()}><strong>Humidity</strong></p> 
+               <p className='secondHeaderContent' onClick={()=>handleWind()}><strong>Wind</strong></p> 
+           </div>
+          
+          <p>Sunrise: {props.weatherData.sys.sunrise}</p>
+          <p>Sunset: {props.weatherData.sys.sunset}</p>
+          <p>Description: {props.weatherData.weather[0].description}</p>
+      
+    </div>
   )}
   
   export default Weather;
